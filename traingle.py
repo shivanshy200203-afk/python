c5d444bb5b47fca1162e1bf611a599a6bcc90cc3
shivansh200203-afk <shivanshy200203@gmail.com>
2026-06-28 19:45:15 +0530

    traingle

---
+++ traingle.py
@@ -0,0 +1,11 @@
+# write a program to find the area of triangle
+# formula of area of triangle = 1/2* Base * Heigh
+
+Base = float (input('Enter the base'))
+
+Height = float(input('Enter the height'))
+
+# apply formula 
+area = 0.5 * Base* Height
+
+print(area)
