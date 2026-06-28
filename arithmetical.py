85c1d5c39189796630eb24b3c09159dedf56227b
shivansh200203-afk <shivanshy200203@gmail.com>
2026-06-28 19:06:11 +0530

    arithmetic problem

---
+++ arithmetical.py
@@ -0,0 +1,12 @@
+#Write a Python program to do arithmetical operations addition and division
+
+num1 = int(input('enter the first number'))
+num2 = int(input('enter the secound number'))
+# addition 
+print(num1 + num2)
+
+#division
+num3 =float(input('enter the first number'))
+num4 =float(input('enter the secound number'))
+
+print(num3 / num4)
