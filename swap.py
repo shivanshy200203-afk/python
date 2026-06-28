8a097f692a92044b52b6c0e959e98e09ef1f542f
shivansh200203-afk <shivanshy200203@gmail.com>
2026-06-28 20:43:15 +0530

    swap

---
+++ swap.py
@@ -0,0 +1,11 @@
+# write a python program to swap two variables.
+
+a = int(input('enter the number a '))
+b = int(input (' enter the number b'))
+
+temp= a
+a = b
+b = temp
+
+print("a =", a)
+print("b= ", b)
