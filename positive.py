acab8f30f2f8d73ccbce9f6aca076d03b7736df2
shivansh200203-afk <shivanshy200203@gmail.com>
2026-06-29 13:15:42 +0530

    msg

---
+++ positive.py
@@ -0,0 +1,9 @@
+# write a program check if a Number is Positive, negative and zero.
+
+num = int(input('enter the number'))
+if num > 0 :
+    print('positive number')
+elif num == 0 :
+    print('zero number')
+else:
+    print('negative number')
