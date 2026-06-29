87f53bf2957be3e981bb95e6fa1c7adc42939fdb
shivansh200203-afk <shivanshy200203@gmail.com>
2026-06-29 12:24:14 +0530

    even

---
+++ num2.py
@@ -0,0 +1,8 @@
+# write a python program to check if is Number is odd or even
+
+num = int(input('enter the number'))
+
+if num % 2 == 0 :
+ print("This is the even number")
+else :
+ print(" This is the odd number")
