eb0dc5038ae4ec9a80ec5627d6d1d85e4be3766a
shivansh200203-afk <shivanshy200203@gmail.com>
2026-07-02 21:48:35 +0530

    divisible number

---
+++ divisible.py
@@ -0,0 +1,8 @@
+# write a program that will tell ehether the given number is divisible by 3 & 6.
+
+n = int(input('enter the number '))
+
+if n%3==0 and n%6==0:
+    print('number is divisible by 3 % 6')
+else:
+    print('number is not divisibe by 3 % 6')
