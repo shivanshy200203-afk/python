42162ce7f3bb4e00ac6b70a3973eb85f8c38e4b3
shivansh200203-afk <shivanshy200203@gmail.com>
2026-07-02 20:13:56 +0530

    factoril number

---
+++ factorial.py
@@ -0,0 +1,15 @@
+# Write a program to find the factorial value of any number 
+
+n = int(input('enter the number'))
+
+fact = 1
+
+for i in range(1,n+1):
+    fact = fact*i
+
+    print(fact)
+
+
+
+
+
