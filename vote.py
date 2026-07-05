557ffc53d21992121352733fceb6e4e14f680725
shivansh200203-afk <shivanshy200203@gmail.com>
2026-07-05 23:13:46 +0530

    vote

---
+++ vote.py
@@ -0,0 +1,7 @@
+#write a program to check whether a person is eligible to vote (age> 18)
+age = int(input('enter th number' ))
+
+if age >= 18:
+ print('Eligile to vote')
+else:
+ print('not Eligile')
