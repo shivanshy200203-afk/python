c6937631e8845a1903ff18943421de62ed98881e
shivansh200203-afk <shivanshy200203@gmail.com>
2026-07-09 20:21:48 +0530

    number of list

---
+++ number.py
@@ -0,0 +1,9 @@
+# number = [10,20, 30,40,50]
+# third element : 30
+# length of list : 5 
+# is the list empty ? false
+
+number =[10,20,30,40,50]
+print(number[2])
+print(len(number))
+print(not number)
