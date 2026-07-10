43ede7d873d8c19a7211321e9b504b3dea88ed16
shivansh200203-afk <shivanshy200203@gmail.com>
2026-07-10 12:25:47 +0530

    even odd

---
+++ number6.py
@@ -0,0 +1,15 @@
+# count  even and odd number
+
+number = [10, 21,4,45,66,93,11]
+even = 0
+odd = 0
+for i in number:
+
+ if i % 2 == 0:
+    even = even + 1
+ else:
+   odd = odd + 1
+
+print(even)
+print(odd)
+
