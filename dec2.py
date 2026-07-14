58fc1b4706e08a2a87b473f480fb8a1d24ab64ac
shivansh200203-afk <shivanshy200203@gmail.com>
2026-07-14 19:38:59 +0530

    Dictionary Operations

---
+++ dec2.py
@@ -0,0 +1,12 @@
+# Dictionary Operations
+car = {"brand": "Toyota", "model": "Camry", "year": 2022, "color": "blue"}
+
+car.pop("color")
+
+print(car)
+
+print(car.items() )
+
+print("brand" in car)
+
+print("color" in car)
